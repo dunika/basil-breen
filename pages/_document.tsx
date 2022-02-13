d9a8ad862d4a8848fdaa6d3918013e973f3877f8
Donncha Breen <donncha.breen@gmail.com>
2022-02-13 21:51:47 +0000

    Add app icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,7 +12,13 @@ export default class AppDocument extends Document {
       <Html lang="en">
         <Head>
           <meta name="theme-color" content={theme.palette.primary.main} />
-          <link rel="icon" href="/favicon.ico" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+          <link rel="manifest" href="/favicons/site.webmanifest" />
+          <link rel="mask-icon" href="/favicons/safari-pinned-tab.svg" color="#fff" />
+          <meta name="msapplication-TileColor" content="#fff" />
+          <meta name="theme-color" content="#ffffff" />
           <link
             rel="stylesheet"
             href="https://fonts.googleapis.com/css?family=Raleway:300,400,500,700&display=swap"
